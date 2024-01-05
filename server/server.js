03341ddc8f27ffe9bcf4ef7e97e49c31e35a5ff9
Pavithira Primraj Ravichandra <priyaprimraj@gmail.com>
2024-01-05 06:57:02 +0530

    backend server

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const cors=require("cors");
+
+const app=express();
+
+var corsOpt={
+	origin:"http://localhost:3000"
+};
+
+app.use(cors(corsOpt));
+
+//parse request of content type
+app.use(express.json());
+
+//parse request of content type-application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}));
+
+const db=require("./myrate/models");
+db.mongoose
+ .connect(db.url,{
+	 useNewUrlParser:true,
+	 useUnifiedTopology:true
+ })
+ .then(() => {
+	 console.log("successfully connected to MongoDB!");	 
+ })
+ .catch(err => {
+	console.log("cannot connect to MongoDB!",err);
+process.exit();	
+ });
+ 
+ //simple route
+ app.get("/",(req,res)=>{
+	 res.json({message:"welcome to our sample restful application"});
+ });
+ require("./myrate/route/rating.route")(app);
+ //set port,listen for requests
+ const PORT=3000;
+ app.listen(PORT,() => {
+	console.log(`server is running on port ${PORT}`);
+
+ });
+ 
