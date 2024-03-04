69b9b1830ab62b533d50089787bce703ad8e5a8c
Pavithira Primraj Ravichandra <priyaprimraj@gmail.com>
2024-03-04 08:56:54 +0530

    models for rating

---
+++ server/rate/models/index.js
@@ -0,0 +1,11 @@
+const dbConfig = require("../config/db.config.js");
+
+const mongoose = require("mongoose");
+
+const db = {};
+db.mongoose = mongoose;
+db.url      = dbConfig.url;
+db.rating = require("./rating.model.js")(mongoose);
+
+module.exports = db;
+    
