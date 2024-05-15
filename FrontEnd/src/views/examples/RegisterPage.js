b5cc7f9c7bf4be0daf4a6c10eb8a2dbccb889310
Mohamed Ansaf <120506142+mohamedansaf20@users.noreply.github.com>
2024-05-16 00:04:54 +0530

    Update RegisterPage.js

--- FrontEnd/src/views/examples/RegisterPage.js
+++ FrontEnd/src/views/examples/RegisterPage.js
@@ -6,3 +6,15 @@ import bcrypt from 'bcryptjs';
 // reactstrap components
 import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
 
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Mynavlog from "components/Navbars/Mynavlog.js";
+import Home from "./Home";
+function RegisterPage() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
