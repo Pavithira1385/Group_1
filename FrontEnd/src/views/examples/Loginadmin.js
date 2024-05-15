16fdfbbeb7beeb6119fe62cf9ea656e0b1ac427f
Mohamed Ansaf <120506142+mohamedansaf20@users.noreply.github.com>
2024-05-16 00:02:00 +0530

    Update Loginadmin.js
    second commit

--- FrontEnd/src/views/examples/Loginadmin.js
+++ FrontEnd/src/views/examples/Loginadmin.js
@@ -5,3 +5,17 @@ import { useState } from "react";
 import bcrypt from 'bcryptjs';
 // reactstrap components
 import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Mynavlog from "components/Navbars/Mynavlog.js";
+import Home from "./Home";
+import Admin from "./AdminDashboard";
+function RegisterPage() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("register-page");
+    return function cleanup() {
+      document.body.classList.remove("register-page");
+    };
+  });
