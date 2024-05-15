7f6fba700f240aaf34003ae2815df9d773f65c51
Mohamed Ansaf <120506142+mohamedansaf20@users.noreply.github.com>
2024-05-16 00:01:25 +0530

    Update Loginadmin.js

--- FrontEnd/src/views/examples/Loginadmin.js
+++ FrontEnd/src/views/examples/Loginadmin.js
@@ -1,1 +1,7 @@
 
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import bcrypt from 'bcryptjs';
+// reactstrap components
+import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
