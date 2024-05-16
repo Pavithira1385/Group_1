ad914f9e17da35ef59df58f98f1b76df2abae712
Paranitha Anantharajah <87082594+Paranitha-07@users.noreply.github.com>
2024-05-16 07:58:45 +0530

    Update ContactUs.js

--- FrontEnd/src/views/examples/ContactUs.js
+++ FrontEnd/src/views/examples/ContactUs.js
@@ -9,4 +9,23 @@ const ContactUs = () => {
     style={{
        backgroundImage: "url(" + require("assets/img/ln2.jpg") + ")",
     }}>
+   <div className="contact-details">
+        <h2>Contact Us</h2>
+        <p className="contact-text">
+          Feel free to get in touch with us using the information below. We look forward to hearing from you!
+        </p>
+        <div className="company-info">
+          <h3>TRAVELERS' WORLD</h3>
+          <p>123 Main Street</p>
+          <p>Pampaimadu, Vavuniya</p>
+          <p>Sri Lanka</p>
+          <p>Phone: 065 2222222</p>
+          <p>Email: travelers'world@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
 
