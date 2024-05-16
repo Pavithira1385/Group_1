232b9a7e987604e68e129ecfae9d39b4c59b15ba
Pavithira Primraj Ravichandra <87083427+Pavithira1385@users.noreply.github.com>
2024-05-16 05:36:44 +0530

    Update RatingPage.js

--- FrontEnd/src/views/examples/RatingPage.js
+++ FrontEnd/src/views/examples/RatingPage.js
@@ -40,3 +40,73 @@ function Rating() {
       alert('Failed to submit suggestion. Please try again.');
     }
   };
+   return (
+    <>
+      <Homenav />
+      <div
+        className="page-header"
+        style={{
+           backgroundImage: "url(" + require("assets/img/ln2.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" lg="4">
+              <Card className="card-register ml-auto mr-auto">
+                <div style={{ textAlign: 'center', marginTop: '50px' }}>
+                  <h2>Rate this page</h2>
+                  <label>Your Comment</label>
+                  <Input 
+                    placeholder="Type here" 
+                    type="text" 
+                    value={suggestions}
+                    onChange={(e) => setSuggestions(e.target.value)}
+                  />
+                  {[...Array(5)].map((_, index) => {
+                    const ratingValue = index + 1;
+                    return (
+                      <span
+                        key={ratingValue}
+                        style={{ cursor: 'pointer', color:'yellow' ,fontSize:50}}
+                        onClick={() => handleRatingClick(ratingValue)}
+                        onMouseEnter={() => handleMouseEnter(ratingValue)}
+                        onMouseLeave={handleMouseLeave}
+                      >
+                        {hoverRating ? (
+                          ratingValue <= hoverRating ?  '★'  : '☆'
+                        ) : (
+                          ratingValue <= rating ? '★' : '☆'
+                        )}
+                      </span>
+                    );
+                  })}
+                  <br />
+                  {rating ? <p>You rated this page: {rating} stars</p> : <p>Please rate this page</p>}
+                  {submissionSuccess && <p>Thank you for your feedback!</p>}
+                </div>
+                <Button 
+                  block 
+                  className="btn-round" 
+                  color="info" 
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </Button>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+        <div className="footer register-footer text-center">
+          <h6>
+            © {new Date().getFullYear()}, made with{" "}
+            <i className="fa fa-heart heart" /> by Group one
+          </h6>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Rating;
+
