c6e82f238d5ca1b62a18e2e095eb85db7bc0ba4c
Paranitha Anantharajah <87082594+Paranitha-07@users.noreply.github.com>
2024-05-16 07:58:19 +0530

    Update ContactUs.js

--- FrontEnd/src/views/examples/ContactUs.js
+++ FrontEnd/src/views/examples/ContactUs.js
@@ -2,3 +2,11 @@ import React from 'react';
 import 'assets/css/ContactUs.css';
 import Nav from 'components/Navbars/RentNav';
 const ContactUs = () => {
+   return (
+    <>
+    <Nav/>
+    <div className="page-header"
+    style={{
+       backgroundImage: "url(" + require("assets/img/ln2.jpg") + ")",
+    }}>
+
